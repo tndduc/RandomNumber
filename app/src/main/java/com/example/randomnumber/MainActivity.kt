2a2b1863beea7dcf1524070eaaf2bf16930a9e81
Tran Ngoc Duc <tnd.duc@gmail.com>
2022-01-19 11:25:04 +0700

    update 1

--- app/src/main/java/com/example/randomnumber/MainActivity.kt
+++ app/src/main/java/com/example/randomnumber/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.randomnumber
-
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.buttonRandom)
-        val toast = Toast.makeText(this, "Duck dep trai", Toast.LENGTH_SHORT)
-        rollButton.setOnClickListener {
-            toast.show()
-        }
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
